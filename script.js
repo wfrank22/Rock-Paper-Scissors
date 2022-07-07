c081158612d3381379a2e8e7a05bc6631ff135b8
wfrank22 <walkerfrank22@gmail.com>
2022-07-06 20:42:28 -0700

    Add game() & further updates to playRound()

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//select rock, paper, or scissors randomly
 function computerPlay(){
     let selector = Math.floor(Math.random() * 3) + 1;
     if (selector === 1){
@@ -6,45 +7,63 @@ function computerPlay(){
     else if (selector === 2){
         return 'Paper';
     }
-    else if (selector === 3){
+    else {
         return 'Scissors';
-    }
-    else{
-        return 'Something has gone wrong.'
     }
 }
 
+//compare player selection with computer selection and announce winner or tie
 function playRound(playerSelection, computerSelection){
+    //convert to lower case so strings can be compared
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
     if (playerSelection === computerSelection){
         return 'Tie! Play again.';
     }
     if (playerSelection === 'rock'){
-        if (computerSelection === 'scissors'){
+        if (computerSelection === 'Scissors'){
             return 'You Win! Rock beats Scissors';
         }
         else {
-            return 'You Lose! ' + computerSelection + ' beats Rock';
+            return 'You Lose! ' + capitalize(computerSelection) + ' beats Rock';
         }
     }
     if (playerSelection === 'paper'){
-        if (computerSelection === 'rock'){
+        if (computerSelection === 'Rock'){
             return 'You Win! Paper beats Rock';
         }
         else {
-            return 'You Lose! ' + computerSelection + ' beats Paper';
+            return 'You Lose! ' + capitalize(computerSelection) + ' beats Paper';
         }
     }
     if (playerSelection === 'scissors'){
-        if (computerSelection === 'paper'){
+        if (computerSelection === 'Paper'){
             return 'You Win! Scissors beats Paper';
         }
         else {
-            return 'You Lose! ' + computerSelection + ' beats Scissors';
+            return 'You Lose! ' + capitalize(computerSelection) + ' beats Scissors';
         }
     }
 }
 
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
-console.log(computerPlay);
+function game(){
+    for (let i = 0; i < 5; i++){
+        let roundWinner = playRound();
+        console.log(roundWinner)
+    }
+    console.log()
+}
+
+//capitalize first letter in string
+function capitalize(a){
+    a = a.toLowerCase();
+    let firstLetter = a.substring(0, 1);
+    let firstLetterCap = firstLetter.toUpperCase();
+    a = a.replace(firstLetter, firstLetterCap)
+    return a;
+}
+
+let playerSelection = 'ROcK';
+let computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
